aad71f0258129b6ac3981893161935b909464178
gabriel <gabvcampaner@gmail.com>
2024-11-25 13:53:04 -0300

    feat: first commit

---
+++ src/main/java/com/gvc/thinktank_dev/ThinktankDevApplication.java
@@ -0,0 +1,13 @@
+package com.gvc.thinktank_dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThinktankDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThinktankDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gvc/thinktank_dev/ThinktankDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gvc.thinktank_dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThinktankDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
